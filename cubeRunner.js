22e3e553a0c21b9353ae681fea49a78d75be6424
Karen Li <karenli.614@gmail.com>
2017-02-17 23:54:02 -0800

    Generate single cube with outline

--- cubeRunner.js
+++ cubeRunner.js
@@ -3,6 +3,75 @@
 // SET UP WEBGL AND HTML CANVAS
 var canvas;
 var gl;
+var program;
+
+// DATA STORAGE FOR POINTS, COLOURS, ETC.
+var points = [];
+var numVertices = 36;
+
+var outlinePoints = [];
+var numOutlinePoints = 24;
+
+// VARIABLES NEEDED FOR PHONG LIGHTING
+// the light is in front of the cube, which is located st z = 5
+var lightPosition = vec4(15, 10, 25, 0.0 );
+var lightAmbient = vec4(0.8, 0.8, 0.8, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+// variables needed for the material of the cube
+var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 1.0, 1.0, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 100.0;
+
+var ambientProduct, diffuseProduct, specularProduct;
+var viewerPos;
+var normalsArray = [];
+
+var vertices =    // manually plan out unit cube
+[
+    vec4( -0.5, -0.5, +0.5, 1.0 ),   
+    vec4( -0.5, +0.5, +0.5, 1.0 ),
+    vec4( +0.5, +0.5, +0.5, 1.0 ),
+    vec4( +0.5, -0.5, +0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5, +0.5, -0.5, 1.0 ),
+    vec4( +0.5, +0.5, -0.5, 1.0 ),
+    vec4( +0.5, -0.5, -0.5, 1.0 )
+];
+
+var colors = 
+[
+    [ 1.0, 0.0, 0.0, 1.0 ],  // red
+    [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+    [ 0.0, 1.0, 0.0, 1.0 ],  // green
+    [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+    [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+    [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+    [ 0.0, 0.5, 0.5, 1.0 ],  // turquoise
+    [ 0.9, 0.4, 0.5, 1.0 ],  // pink
+    [ 1.0, 1.0, 1.0, 1.0 ]  // white
+];
+
+// DECLARE VARIABLES FOR UNIFORM LOCATIONS
+var modelTransformMatrixLoc;
+var cameraTransformMatrixLoc;
+var perspectiveMatrixLoc;
+var currentColourLoc;
+
+// INITIALIZE ALL TRANSFORMATION MATRICES
+var modelTransformMatrix = mat4();  // identity matrix
+var perspectiveMatrix = mat4();
+var cameraTransformMatrix = mat4();
+
+// SET UP BUFFER AND ATTRIBUTES
+var vPosition;
+var vBuffer;
+var vOutlineBuffer;
+
+// INITIALIZE MISCELLANEOUS VARIABLES 
+var currentFOV = 50;   // adjust this later for narrow or width FOV
 
 window.onload = function init()   
 {
@@ -18,16 +87,149 @@ window.onload = function init()
     gl.enable(gl.DEPTH_TEST);
 
     // LOAD SHADERS AND INITIALIZE ATTRIBUTE BUFFERS
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );  // compile and link shaders, then return a pointer to the program
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );  // compile and link shaders, then return a pointer to the program
     gl.useProgram( program ); 
 
+    // BUFFER AND ATTRIBUTE FOR THE CUBE POINTS
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.DYNAMIC_DRAW );
+
+    vPosition = gl.getAttribLocation( program, "vPosition" ); 
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );  
+    gl.enableVertexAttribArray( vPosition );
+
+    // BUFFER AND ATTRIBUTES FOR THE CUBE OUTLINE POINTS
+    vOutlineBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vOutlineBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(outlinePoints), gl.DYNAMIC_DRAW );
+
+    
+
+    // SET VALUES FOR UNIFORMS FOR SHADERS
+    modelTransformMatrixLoc = gl.getUniformLocation(program, "modelTransformMatrix"); 
+    cameraTransformMatrixLoc = gl.getUniformLocation(program, "cameraTransformMatrix");
+    perspectiveMatrixLoc = gl.getUniformLocation(program, "perspectiveMatrix");
+    currentColourLoc = gl.getUniformLocation(program, "currentColour");
+
+    // INITIALIZE THE TRANSFORMATION MATRICES    
+    // gl.uniformMatrix4fv(modelTransformMatrixLoc, false, flatten(modelTransformMatrix)); 
+    // move cube away from the origin to check if perspective is correct
+    // TODO: remove this
+    modelTransformMatrix = mult(modelTransformMatrix, translate(5, 5, 5));
+    gl.uniformMatrix4fv(modelTransformMatrixLoc, false, flatten(modelTransformMatrix));
+
+    // want to move camera in the +z direction since you are looking down the -z axis
+    // in reality, since we are taking the inverse matrix, we are moving all the objects in the -z direction
+    cameraTransformMatrix = mult(cameraTransformMatrix, inverse(translate(0, 0, 50)));
+    gl.uniformMatrix4fv(cameraTransformMatrixLoc, false, flatten(cameraTransformMatrix));
+
+    // apply symmetric perspective projection
+    perspectiveMatrix = perspective(currentFOV, 1, 1, 100);
+    gl.uniformMatrix4fv(perspectiveMatrixLoc, false, flatten(perspectiveMatrix));
+
+    // populate the points array 
+    generateCube();
+
+    // populate the outline points array
+    generateCubeOutline();
+
     render(0);
+}
+
+// called for each face of the cube
+function quad( a, b, c, d )
+{
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = cross(t1, t2);
+     var normal = vec3(normal);
+
+     points.push(vertices[a]);
+     normalsArray.push(normal);
+
+     points.push(vertices[b]);
+     normalsArray.push(normal);
+
+     points.push(vertices[c]);
+     normalsArray.push(normal);
+
+     points.push(vertices[a]);
+     normalsArray.push(normal);
+
+     points.push(vertices[c]);
+     normalsArray.push(normal);
+
+     points.push(vertices[d]);
+     normalsArray.push(normal);
+}
+
+// generate points for the cube
+function generateCube()
+{
+    // RHR traversal from vertex 1 to 0 to 3 to 2, only colors one side
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+// generate vertices for the cube outline
+function generateCubeOutline() {
+    // generate lines for front face of the cube
+    outlinePoints.push(vertices[0]);
+    for (var i = 1; i < 4; i++) {
+        outlinePoints.push(vertices[i]);
+        outlinePoints.push(vertices[i]);
+    }
+    outlinePoints.push(vertices[0]);
+    // generate lines for the back face of the cube
+    outlinePoints.push(vertices[4]);
+    for (var j = 5; j < 8; j++) {
+        outlinePoints.push(vertices[j]);
+        outlinePoints.push(vertices[j]);
+    }
+    outlinePoints.push(vertices[4]);
+    // generate four lines to connect the top face to the bottom face
+    for (var k = 0; k < 4; k++) {
+        outlinePoints.push(vertices[k]);
+        outlinePoints.push(vertices[k+4]);
+    }
+}
+
+// draw the cube outline in white
+function drawOutline() {
+    // bind the current buffer that we want to draw (the one with the points)
+    gl.bindBuffer( gl.ARRAY_BUFFER, vOutlineBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(outlinePoints), gl.DYNAMIC_DRAW );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );  // tell attribute how to get data out of buffer and binds current buffer to the attribute; vPosition will always be bound to vBuffer now
+    gl.enableVertexAttribArray( vPosition );
+    gl.uniform4fv(currentColourLoc, colors[8]);  // make the outline white 
+    gl.drawArrays( gl.LINES, 0, numOutlinePoints );
+}
+
+function drawCube() {
+    // bind the current buffer that we want to draw (the one with the points)
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.DYNAMIC_DRAW );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );  // tell attribute how to get data out of buffer and binds current buffer to the attribute; vPosition will always be bound to vBuffer now
+    gl.enableVertexAttribArray( vPosition );
+    // change the colour for the cube
+    // TODO: change it from default to cyan
+    gl.uniform4fv(currentColourLoc, colors[5]); 
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, numVertices );  // draw cube using triangle strip
 }
 
 function render(timeStamp) 
 {
     // clear colour buffer and depth buffer
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // draw a single cube
+    drawOutline();
+    drawCube();
 
     // render again (repeatedly as long as program is running)
     requestAnimationFrame( render );
