978c895ab5efed474300cb342a1dbc10d59ee850
Katrina Wijaya <katrina@thewijayas.com>
2017-02-20 05:40:21 -0800

    Deleted duplicate code from splitting files

--- cubeRunner.js
+++ cubeRunner.js
@@ -64,14 +64,21 @@ var enableTexture = false;  // by default we do not use textures
 var texCoords =    // mapping between the texture coordinates (range from 0 to 1) and object
 [
     // square, so just use two triangles
-    // triangle #1
-    vec2(0.0,  1.0),
-    vec2(0.0,  0.0),
-    vec2(1.0,  0.0),
-    // triangle #2
-    vec2(0.0,  1.0),
-    vec2(1.0,  0.0),
-    vec2(1.0,  1.0)
+    // // triangle #1
+    // vec2(0.0,  1.0),
+    // vec2(0.0,  0.0),
+    // vec2(1.0,  0.0),
+    // // triangle #2
+    // vec2(0.0,  1.0),
+    // vec2(1.0,  0.0),
+    // vec2(1.0,  1.0)
+
+    vec2(0, 3), //1
+    vec2(0, 0), //0
+    vec2(3, 0), //3
+    vec2(0, 3), //1
+    vec2(3, 0), //3
+    vec2(3, 3)  //2
 ];
 
 // DECLARE VARIABLES FOR UNIFORM LOCATIONS
@@ -242,197 +249,6 @@ window.onload = function init()
 // returns whether value is a power of 2 or not
 function isPowerOf2(value) {
     return (value & (value - 1)) == 0;
-}
-
-// called for each face of the cube
-function quad( a, b, c, d )
-{
-     var t1 = subtract(vertices[b], vertices[a]);
-     var t2 = subtract(vertices[c], vertices[b]);
-     var normal = cross(t1, t2);
-
-     var vertexOrder = [a, b, c, a, c, d];
-
-     for (var i = 0; i < 6; i++) {
-        points.push(vertices[vertexOrder[i]]);
-        normalsArray.push(normal);
-     }
- }
-
-// generate points for the cube
-function generateCube()
-{
-    // RHR traversal from vertex 1 to 0 to 3 to 2, only colors one side
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-}
-
-// generate vertices for the cube outline
-function generateCubeOutline() {
-    // generate lines for front face of the cube
-    outlinePoints.push(vertices[0]);
-    for (var i = 1; i < 4; i++) {
-        outlinePoints.push(vertices[i]);
-        outlinePoints.push(vertices[i]);
-    }
-    outlinePoints.push(vertices[0]);
-    // generate lines for the back face of the cube
-    outlinePoints.push(vertices[4]);
-    for (var j = 5; j < 8; j++) {
-        outlinePoints.push(vertices[j]);
-        outlinePoints.push(vertices[j]);
-    }
-    outlinePoints.push(vertices[4]);
-    // generate four lines to connect the top face to the bottom face
-    for (var k = 0; k < 4; k++) {
-        outlinePoints.push(vertices[k]);
-        outlinePoints.push(vertices[k+4]);
-    }
-}
-
-// Generate the random starting x positions of a line of cubes and push this into the array of all cube line positions; also pushes the starting position (always -cameraPositionZAxis since they start at the end of the screen)
-function generateNewCubeLine()
-{
-    // Generate a random number of cubes in the line (7-10)
-    var numCubes = 7 + Math.floor((Math.random() * 3) + 1);
-    // Section the path into equal length segments
-    var sectionPathWidth = Math.floor( (canvas.width/12) / numCubes );  // we only want to use one quarter of the canvas width so that the cubes are generated near the middle of the screen
-
-    // Holds the unique x positions for the numCubes X positions
-    var positions = [];
-    var colours = [];
-
-    for( var i = 0; i < numCubes; i++ )
-    {
-        // which section of the canvas
-        var whichSection = (i * sectionPathWidth);
-        // what index in the section of the canvas
-        var indexInSection = Math.floor( Math.random() * (sectionPathWidth - 2)) + 1;
-        // initial offset on canvas
-        var initialOffset = - canvas.width / 24;
-        var randomPosition = whichSection + indexInSection + initialOffset;
-        positions.push( randomPosition );
-
-        // pick a random colour for the cube (index between 0 and 3)
-        var cubeColour = Math.floor((Math.random() * (4)));
-        colours.push(cubeColour);
-    }
-
-    // Push the array of X positions in the cube line to the array of all cube line positions
-    allCubeLineXPositions.push( positions );
-    allCubeLineZPositions.push( -cameraPositionZAxis );
-    allCubeColours.push(colours);
-}
-
-// draw the cube outline in white
-function drawOutline() {
-    // bind the current buffer that we want to draw (the one with the points)
-    gl.bindBuffer( gl.ARRAY_BUFFER, vOutlineBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(outlinePoints), gl.STATIC_DRAW );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );  // tell attribute how to get data out of buffer and binds current buffer to the attribute; vPosition will always be bound to vBuffer now
-    gl.enableVertexAttribArray( vPosition );
-    if (isForBorder)
-    {
-        gl.uniform4fv(currentColourLoc, colors[0]);  // make the outline while
-        isForBorder = 0;
-    }
-    else
-    {
-        gl.uniform4fv(currentColourLoc, colors[4]);  // make the outline black
-    }
-    gl.drawArrays( gl.LINES, 0, numOutlinePoints );
-}
-
-// draw the cube with a specified colour
-function drawCube(colourIndex) {
-    // bind the current buffer that we want to draw (the one with the points)
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );  // tell attribute how to get data out of buffer and binds current buffer to the attribute; vPosition will always be bound to vBuffer now
-    gl.enableVertexAttribArray( vPosition );
-    // change the colour for the cube (want to index between 0 and 3)
-    if (!isAllWhite)
-        gl.uniform4fv(currentColourLoc, colors[colourIndex]);
-    else
-        gl.uniform4fv(currentColourLoc, colors[0]);  // set the cubes all white
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );  // draw cube using triangle strip
-}
-
-// THIS WORKS BUT ONLY DRAWS ONE CUBE
-// draw and move all cubes forward at a constant speed
-function drawAndMoveCubes() {
-    // draw a single cube
-    transformCube(1, -cameraPositionZAxis);
-    // apply the camera transformation so the cubes all move forward
-    cameraTransformMatrix = mult(translate(0, 0, amountToMove), cameraTransformMatrix);
-    gl.uniformMatrix4fv(cameraTransformMatrixLoc, false, flatten(cameraTransformMatrix));
-    drawOutline();
-    drawCube();
-}
-
-// THIS WORKS FOR ONE LINE OF CUBES
-// Draw line of cubes and transform them
-function drawAndMoveAllCubes()
-{
-    // iterate through each row of cubes (one cube line at a time)
-    for ( var r = 0; r < allCubeLineXPositions.length; r++ )
-    {
-        // get the new z position for this row of cubes by adding amountToMove to the original z position
-         allCubeLineZPositions[r] = allCubeLineZPositions[r] + amountToMove;
-
-        // iterate through all the cubes on a single cube line
-        for ( var c = 0; c < allCubeLineXPositions[r].length; c++ )
-        {
-            // move the cube to the correct position
-            transformCube( allCubeLineXPositions[r][c],  allCubeLineZPositions[r] );
-            // draw the cubes and outlines
-            drawOutline();
-            // set the colour for the cube
-            drawCube(allCubeColours[r][c]);
-        }
-    }
-}
-
-function drawBorder() {
-    // iterate through the whole length of the canvas and draw borders made of cubes on the sides
-    for (var i = -cameraPositionZAxis; i < cameraPositionZAxis; i++) {
-        // draw cube on left side
-        transformCube( -canvas.width/12, i );
-        drawOutline();  // draw the outline for the cube
-        drawCube(4);  // draw the cube as dark grey
-        // draw cube on right side
-        transformCube( canvas.width/12, i );
-        isForBorder = 1;
-        drawOutline();  // draw the outline for the cube
-        drawCube(4);  // draw the cube as dark grey
-    }
-}
-
-// modify and apply the model transform matrix for the cubes
-function transformCube(xPosition, zPosition) {
-    // move the cubes to the correct x and z axis positions
-    modelTransformMatrix = translate(xPosition, 0, zPosition);
-    gl.uniformMatrix4fv(modelTransformMatrixLoc, false, flatten(modelTransformMatrix));
-}
-
-function destroyOutOfRangeCubes() {
-    for ( var i = 0; i < allCubeLineZPositions.length; i++) {
-        // check to see if the z position is past that of the user
-        if (allCubeLineZPositions[i] > cameraPositionZAxis) {
-            // delete the cube's data from both arrays
-            allCubeLineZPositions.shift();
-            allCubeLineXPositions.shift();
-            allCubeColours.shift();
-            // move the iterator back one so you don't miss the next element
-            i--;
-        }
-        else
-            break;  // since the z values decrease as you go through the array (later cube lines have smaller z values), then if the z position is not past the user for this cube line, then the rest of the cube lines will not be past the user either
-    }
 }
 
 // called repeatedly to render and draw our scene
